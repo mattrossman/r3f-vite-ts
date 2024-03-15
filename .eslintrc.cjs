fc1c0b40a7a80245802544ede7abfff29b0c38e5
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2024-03-15 08:53:00 -0400

    Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,24 @@
 module.exports = {
-	root: true,
-	env: { browser: true, es2020: true },
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:react-hooks/recommended",
-	],
-	ignorePatterns: ["dist", ".eslintrc.cjs"],
-	parser: "@typescript-eslint/parser",
-	plugins: ["react-refresh"],
-	rules: {
-		"no-constant-condition": ["error", { checkLoops: false }],
-		"react-refresh/only-export-components": [
-			"warn",
-			{ allowConstantExport: true },
-		],
-		"@typescript-eslint/no-unused-vars": [
-			"warn",
-			{ argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-		],
-		"@typescript-eslint/consistent-type-imports": "warn",
-	},
-};
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "no-constant-condition": ["error", { checkLoops: false }],
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/consistent-type-imports": "warn",
+  },
+}
